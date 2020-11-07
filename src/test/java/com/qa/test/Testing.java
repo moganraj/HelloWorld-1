7765943900c322f8006e2b92fb150c3690758791
moganraj <moganpondy@gmail.com>
2020-11-06 20:15:48 -0500

    first commit

---
+++ src/test/java/com/qa/test/Testing.java
@@ -0,0 +1,13 @@
+package com.qa.test;
+
+import org.testng.annotations.Test;
+
+public class Testing {
+
+	@Test
+	public void testfn() {
+		System.out.println("--------Hello World--------");		
+	}
+	
+	
+}
